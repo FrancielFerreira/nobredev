e25e51382d3eb19974d60adfa068a6a15786d87c
Franciel Ferreira <72324148+FrancielFerreira@users.noreply.github.com>
2022-06-21 19:04:12 -0300

    bug gasleria

--- praticas/galeria-lightbox/js/modules/color-mode.js
+++ praticas/galeria-lightbox/js/modules/color-mode.js
@@ -1,8 +1,5 @@
 export default function initColorMode() {
   const preload = document.querySelector('.loader_bg') 
   const img = document.querySelector('#galeria')
-  // setTimeout(function(){
-  //         preload.style.display = 'none';
-  // }, 2000); // com setTimout
-  window.addEventListener('load', () => preload.style.display = 'none') // ao carregar a página
+  window.addEventListener('load', () => preload.style.display = 'none') 
 }
--- praticas/galeria-lightbox/js/modules/galeria-lightbox.js
+++ praticas/galeria-lightbox/js/modules/galeria-lightbox.js
@@ -1,51 +1,52 @@
 export default function initGaleria() {
-  const galeria = document.querySelectorAll("#galeria img");
-  const galeriaContainer = document.querySelector(".galeria-container");
-  const galeriaImg = galeriaContainer.querySelector("img");
-  const galeriaImgCaption = galeriaContainer.querySelector("figcaption");
-  const btnCloseGaleria = galeriaContainer.querySelector("#close-galeria");
-  const btnPrev = galeriaContainer.querySelector("#imgPrev");
-  const btnNext = document.querySelector("#imgNext");
-  const btnToogleImg = document.querySelectorAll(".galeria-controls button");
-  const totalImg = galeria.length - 1;
+  onload = ()=> {
+    const galeria = document.querySelectorAll("#galeria img");
+    const galeriaContainer = document.querySelector(".galeria-container");
+    const galeriaImg = galeriaContainer.querySelector("img");
+    const galeriaImgCaption = galeriaContainer.querySelector("figcaption");
+    const btnCloseGaleria = galeriaContainer.querySelector("#close-galeria");
+    const btnPrev = galeriaContainer.querySelector("#imgPrev");
+    const btnNext = document.querySelector("#imgNext");
+    const totalImg = galeria.length - 1;
 
-  function toggleImgAttr(index) {
-    btnNext.addEventListener("click", () => {
-      index++;
-      index > totalImg ? (index = 0) : 0;
+    function toggleImgAttr(index) {
+      btnNext.addEventListener("click", () => {
+        index++;
+        index > totalImg ? (index = 0) : 0;
+        getImgAttr(index);
+      });
+      btnPrev.addEventListener("click", () => {
+        index--;
+        index < 0 ? (index = totalImg) : 0;
+        getImgAttr(index);
+      });
+    }
+
+    function getImgAttr(index) {
+      const currentImgSrc = galeria[index].getAttribute("src");
+      const currentImgAlt = galeria[index].getAttribute("alt");
+      galeriaContainer.classList.add("ativo");
+      galeriaImg.setAttribute("src", currentImgSrc);
+      galeriaImg.setAttribute("alt", currentImgAlt);
+      galeriaImgCaption.innerText = currentImgAlt;
+    }
+
+    function activeGaleria(index) {
+      galeria.forEach((img) => img.classList.remove("ativo"));
       getImgAttr(index);
-    });
-    btnPrev.addEventListener("click", () => {
-      index--;
-      index < 0 ? (index = totalImg) : 0;
-      getImgAttr(index);
-    });
-  }
+      toggleImgAttr(index);
+    }
 
-  function getImgAttr(index) {
-    const currentImgSrc = galeria[index].getAttribute("src");
-    const currentImgAlt = galeria[index].getAttribute("alt");
-    galeriaContainer.classList.add("ativo");
-    galeriaImg.setAttribute("src", currentImgSrc);
-    galeriaImg.setAttribute("alt", currentImgAlt);
-    galeriaImgCaption.innerText = currentImgAlt;
-  }
+    Array.from(galeria).forEach((img, index) =>
+      img.addEventListener("click", () => activeGaleria(index))
+    );
 
-  function activeGaleria(index) {
-    galeria.forEach((img) => img.classList.remove("ativo"));
-    getImgAttr(index);
-    toggleImgAttr(index);
-  }
+    function closeGaleriaContainer(e) {
+      e.target === this || e.target === btnCloseGaleria
+        ? galeriaContainer.classList.remove("ativo")
+        : 0;
+    }
 
-  Array.from(galeria).forEach((img, index) =>
-    img.addEventListener("click", () => activeGaleria(index))
-  );
-
-  function closeGaleriaContainer(e) {
-    e.target === this || e.target === btnCloseGaleria
-      ? galeriaContainer.classList.remove("ativo")
-      : 0;
-  }
-
-  galeriaContainer.addEventListener("click", closeGaleriaContainer);
+    galeriaContainer.addEventListener("click", closeGaleriaContainer);
+  };
 }
