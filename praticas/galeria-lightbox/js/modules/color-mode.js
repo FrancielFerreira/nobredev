4be542196d4a5b3d22f953ab9a0e6a4d2ff9056c
Franciel Ferreira <72324148+FrancielFerreira@users.noreply.github.com>
2023-09-23 08:40:06 -0300

    atualiza site

--- praticas/galeria-lightbox/js/modules/color-mode.js
+++
@@ -1,5 +0,0 @@
-export default function initColorMode() {
-  const preload = document.querySelector('.loader_bg') 
-  const img = document.querySelector('#galeria')
-  window.addEventListener('load', () => preload.style.display = 'none') 
-}
--- praticas/galeria-lightbox/js/scripts.js
+++
@@ -1,5 +0,0 @@
-import initGaleria from './modules/galeria-lightbox.js';
-import initColorMode from './modules/color-mode.js';
-
-initGaleria();
-initColorMode();
